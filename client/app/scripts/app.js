6fa659a34d3f07f191736fe589c37efcf1146e91
Vivek Poola <vpoola88@gmail.com>
2016-03-19 12:52:00 -0700

    seperate project into client and server repos

---
+++ client/app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name clientApp
+ * @description
+ * # clientApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('clientApp', [
+    'ngAnimate',
+    'ngResource'
+  ]);
