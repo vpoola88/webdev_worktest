65d5c6644925973948bc8e824cf630850a5e47ec
Vivek Poola <vpoola88@gmail.com>
2016-03-19 12:57:32 -0700

    add ui-router

--- client/test/karma.conf.js
+++ client/test/karma.conf.js
@@ -25,6 +25,7 @@ module.exports = function(config) {
       'bower_components/bootstrap/dist/js/bootstrap.js',
       'bower_components/angular-animate/angular-animate.js',
       'bower_components/angular-resource/angular-resource.js',
+      'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
